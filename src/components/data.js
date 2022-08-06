d1641fa4a9587a66bd89319de832a1d5ec9bd671
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2022-08-06 12:34:51 +0500

    pushing my react portfolio to git

---
+++ src/components/data.js
@@ -0,0 +1,66 @@
+import pjimg1 from "./images/project-1.png";
+import pjimg2 from "./images/project-2.png";
+import pjimg3 from "./images/project-3.png";
+import pjimg4 from "./images/project-4.png";
+
+export const projects = [
+  {
+    title: "Simple Todo App",
+    subtitle: "Basic Todo",
+    description:
+      "Simple Todo app allow users to add and remove tasks in a list and mark the tasks status as complete or incomplete, tasks can also be filtered by status.",
+    image: pjimg1,
+    link: "https://rtodo-app.surge.sh/",
+  },
+  {
+    title: "Random User Search",
+    subtitle: "API response",
+    description:
+      "Random user API for learning how to handle API responses in React. The radom API is free use and has unlimited hit count",
+    image: pjimg2,
+    link: "https://random-user-lookup.surge.sh/",
+  },
+  {
+    title: "React Form Validation",
+    subtitle: "Without Any Library",
+    description:
+      "Basic form validation without any use of react validation library. The form response can be viewed instantly after form submission with the help of local storage",
+    image: pjimg3,
+    link: "https://upwork-test.surge.sh/",
+  },
+  {
+    title: "Ecommerce Store",
+    subtitle: "React & Redux",
+    description: "Ecommerce product page for learning redux concepts",
+    image: pjimg4,
+    link: "https://fakestore-app.surge.sh/",
+  },
+];
+
+export const skills = [
+  "HTML5",
+  "CSS3",
+  "JavaScript",
+  "React",
+  "Node",
+  "MongoDB",
+];
+
+export const testimonials = [
+  {
+    id: "1",
+    name: "John Doe",
+    company: "Vistech Solutions",
+    image: "",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum placeat asperiores praesentium maiores veniam hic reprehenderit quaerat eveniet doloremque accusantium!",
+  },
+  {
+    id: "2",
+    name: "Jane Doe",
+    company: "FibrVision",
+    image: "",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum placeat asperiores praesentium maiores veniam hic reprehenderit quaerat eveniet doloremque accusantium!",
+  },
+];
